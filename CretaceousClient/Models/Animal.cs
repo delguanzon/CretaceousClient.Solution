860a1172baa67b54da5d405c9652a543886f92cc
delguanzon <yodelguanzon@gmail.com>
2023-01-19 08:10:50 -0800

    Add PUT Method in API Helper and Animal Model

--- CretaceousClient/Models/Animal.cs
+++ CretaceousClient/Models/Animal.cs
@@ -33,5 +33,17 @@ namespace CretaceousClient.Models
 
             return animal;
         }
+
+        public static void Post(Animal animal)
+        {
+            string jsonAnimal = JsonConvert.SerializeObject(animal);
+            ApiHelper.Post(jsonAnimal);
+        }
+
+        public static void Put(Animal animal)
+        {
+            string jsonAnimal = JsonConvert.SerializeObject(animal);
+            ApiHelper.Put(animal.AnimalId, jsonAnimal);
+        }
     }
 }
