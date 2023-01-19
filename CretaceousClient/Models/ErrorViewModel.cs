69dbded29e1d83d047433bd9d1cfc177aa90ed0c
delguanzon <yodelguanzon@gmail.com>
2023-01-19 07:45:19 -0800

    first commit

---
+++ CretaceousClient/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CretaceousClient.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
